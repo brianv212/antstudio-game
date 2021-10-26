90eca89f15239b68849cea73d3f9019762be38bb
brianv212 <xbrianv00@gmail.com>
2021-10-25 21:04:00 -0700

    First iteration - AntStudio games

---
+++ AntStudio_Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public int playerJumpPower = 10;
+    private float moveX;
+    private bool facingRight = true;
+
+    // isGrounded
+    public bool isGrounded;
+    public LayerMask groundLayers;
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update() {
+        PlayerMove();
+        isGrounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f),
+                                           new Vector2(transform.position.x + 0.5f, transform.position.y - 0.51f),
+                                           groundLayers);
+    }
+
+    void PlayerMove() {
+        // Controls
+        moveX = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(moveX));
+
+        if (Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+
+        if (moveX > 0.0f && facingRight == false) {
+            FlipPlayer();
+        }
+        else if (moveX < 0.0f && facingRight == true) {
+            FlipPlayer();
+        }
+
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer() {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+
+}
