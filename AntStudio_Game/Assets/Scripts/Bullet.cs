d4900f2b91d95f61584437f967b06f8477dfde45
brianv212 <xbrianv00@gmail.com>
2021-11-12 02:15:01 -0800

    Some quick updates

--- AntStudio_Game/Assets/Scripts/Bullet.cs
+++ AntStudio_Game/Assets/Scripts/Bullet.cs
@@ -15,10 +15,10 @@ public class Bullet : MonoBehaviour
     }
 
     void OnTriggerEnter2D (Collider2D hitInfo) {
-        // Enemy enemy = hitInfo.GetComponent<Enemy>();
-        // if (enemy != null) {
-        //     enemy.TakeDamage(damage);
-        // }
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null) {
+            enemy.TakeDamage(damage);
+        }
 
         Destroy(gameObject);
     }
